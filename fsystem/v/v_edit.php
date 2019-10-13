343ff2ab5266a56644efebf4fad1478d65d3ea4c
ilay00 <agidelio@mail.ru>
2019-10-13 18:02:32 +0500

    1301019

---
+++ fsystem/v/v_edit.php
@@ -0,0 +1,14 @@
+<meta charset = "UTF-8">
+<?php
+/**
+ * ������ ���������
+ * ================
+ * $text - ����� ������
+ */
+?>
+
+<form method="post">
+	<textarea name="text"><?=$text?></textarea>
+	<br/>
+	<input type="submit" value="���������" />
+</form>
