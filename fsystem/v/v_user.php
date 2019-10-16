375d9783116a9de3a4146dde8bf1e9ed4aea7725
ilay00 <agidelio@mail.ru>
2019-10-17 00:59:46 +0500

    151019

--- fsystem/v/v_user.php
+++
@@ -1,9 +0,0 @@
-
-
-      <form method="POST" >
-      <p><input type = "text" name = "login" /></p>
-      <p><input type = "password" name = "password" /></p>
-      <p><input type = "submit" value = "За регистриреватся" /></p>
-      </form>
-      
-      
